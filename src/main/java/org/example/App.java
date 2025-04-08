23d3a7cebec0872424632c161e6693e6d43e86f1
doomdex84 <doomdex84@gmail.com>
2025-04-08 19:54:28 +0900

    삭제기능

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,66 +6,102 @@ import java.util.Scanner;
 
 public class App {
 
-    private Scanner sc;
+    public Scanner sc;
+
     public App(Scanner sc) {
         this.sc = sc;
-
     }
 
     public void run() {
-        System.out.println("==Motivation  실행==");
+
+        System.out.println("moti 실행");
 
         int lastId = 0;
 
-        List<Motivation> motivations = new ArrayList<Motivation>();
-        // 저장소
+       List<Motivation> motivations = new ArrayList<Motivation>();
 
         while (true) {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("==Motivation  종료==");
+                System.out.println("moti 종료");
                 break;
-             }else if (cmd.length() == 0) {
-                System.out.println("명령어가 입력되지 않았음");
+            }else if (cmd.length() == 0) {
+                System.out.println("명령어가 입력되지 않음");
                 continue;
             }
-
             if (cmd.equals("add")) {
-
-                int id = lastId + 1;
-                System.out.print("body: ");
+                int id = lastId +1;
+                System.out.print("body : ");
                 String body = sc.nextLine();
-                System.out.print("source: ");
+                System.out.print("source : ");
                 String source = sc.nextLine();
 
-                Motivation motivation = new Motivation(id, body, source);
-                // 각 객체의 내부정보를 채워줌
+                Motivation motivation = new Motivation(id, body, source); //조립
                 motivations.add(motivation);
 
 
-                System.out.printf("%d번 moti가 등록됨\n",id);
+                System.out.println(id + "번 moti 저장");
                 lastId++;
 
-            }else if (cmd.equals("list")) {
-                System.out.println("=".repeat(40));
-                System.out.printf("   번호   /    source    /    body    \n");
 
+
+            }else if (cmd.equals("list")) {
                 if (motivations.size() == 0) {
-                    System.out.println("등록된 moti 없어");
+                    System.out.println("등록된 moti 없음");
                 }
-                else {
-                    System.out.println("1개 이상있음");
+
+                System.out.println("=".repeat(40));
+                System.out.printf("   제목   /  body   /   source   \n");
+
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
+                    System.out.printf("   %d   /  %s   /   %s   \n", motivation.getId(), motivation.getBody(), motivation.getSource());
                 }
+                System.out.println("=".repeat(40));
+
+            }else if(cmd.startsWith("delete")) {
+                String[] cmdBits = cmd.split((" "));
+                int id = Integer.parseInt(cmd.split(" ")[1]);
+
+                Motivation foundmotivation = null;
+
+                for (Motivation motivation : motivations) {
+                    if (motivation.getId() == id) {
+                        motivations.remove(motivation);
+                        break;
+                    }
+
+                   if (foundmotivation == null) {
+                       System.out.println("해당 moti는 없는데?");
+                       return;
+                   }
+
+                   motivations.remove(id-1);
+                    System.out.println(id+"해당 moti 삭제됨");
+
+                }
+                }
+
+            else{
+                System.out.println("사용할 수 없는 명령어");
+            }
+            }
+
             }
         }
-    }
-}
+
 class Motivation {
-    int id;
-    String body;
-    String source;
+    public int id;
+    public String body;
+    public String source;
+
+    public Motivation(int id, String body, String source) {
+        this.id = id;
+        this.body = body;
+        this.source = source;
+    }
 
     @Override
     public String toString() {
@@ -74,12 +110,6 @@ class Motivation {
                 ", body='" + body + '\'' +
                 ", source='" + source + '\'' +
                 '}';
-    }
-
-    public Motivation(int id, String body, String source) {
-        this.id = id;
-        this.body = body;
-        this.source = source;
     }
 
     public int getId() {
